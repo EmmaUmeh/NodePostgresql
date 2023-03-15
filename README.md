68ec21a3c8e7cd41b9a21b13cde280d0fce1050a
EmmaUmeh <Emmachinemerem04@gmail.com>
2023-03-15 09:10:57 -0700

    Commited ReadMe file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Connect Postgresql with Nodejs
+
+This is a simple database store that has list of employee and and salary range.
--- postgres.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE user;
-
-CREATE TABLE userdetails(
-    user_id INT PRIMARY KEY,
-    First_name VARCHAR(30),
-    Last_name VARCHAR(30),
-)
